ca14ff0974cedd2e2e4af59b2617e04ba4f10ef7
tandris <andras.toth@chilisoft.org>
2016-08-14 21:00:02 +0200

    Update heroku delpoy

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Chiliweb.UserSocket do
   # channel "room:*", Chiliweb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
